66eed54cb98da3fdf85c8c5a40640baef011767a
JuliaaMuller <julialm.muller@gmail.com>
2022-02-09 14:14:44 -0500

    SQL injection and parameterized Query

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,16 +7,22 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
+const cohortName = process.argv[2];
+const values = [`%${cohortName}%`];
+const queryString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name LIKE $1
 ORDER BY teacher;
-`)
+`
+
+pool
+.query(queryString, values)
 .then(res => {
+  console.log(queryString)
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
   })
